bebe7daf4e1779a1d78c4e6545519f6b5d0004ef
Alex-Za <38041377+Alex-Za@users.noreply.github.com>
2021-05-09 19:35:25 +0300

    example

--- src/main/java/PrivatCursBot.java
+++ src/main/java/PrivatCursBot.java
@@ -1,28 +1,98 @@
+import com.vdurmont.emoji.EmojiParser;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class PrivatCursBot extends TelegramLongPollingBot {
 
+
+    @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            if (messageText.equals("/start")) {
+                SendMessage message = new SendMessage().setChatId(chatId).setText(messageText);
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (messageText.equals("/pic")) {
+                SendPhoto msg = new SendPhoto().setChatId(chatId).setPhoto("https://trello-attachments.s3.amazonaws.com/5666779e6c24bb815183689c/597f79754996f872478fb5c8/600x264/79a80ac19fe1a4390cba895c920944f2/S61028-20071611.jpg");
+                try {
+                    execute(msg);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (messageText.equals("/markup")) {
+                SendMessage message = new SendMessage().setChatId(chatId).setText("Here is your keyboard");
 
-        update.getUpdateId();
-        String chatId = String.valueOf(update.getMessage().getChatId());
+                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+                List<KeyboardRow> keyboard = new ArrayList<>();
+                KeyboardRow row = new KeyboardRow();
+                row.add("Row 1 Button 1");
+                row.add("Row 1 Button 2");
+                row.add("Row 1 Button 3");
+                keyboard.add(row);
 
-        SendMessage message = new SendMessage().setChatId(chatId);
+                row = new KeyboardRow();
+                row.add("Row 2 Button 1");
+                row.add("Row 2 Button 2");
+                row.add("Row 2 Button 3");
 
-        if (update.getMessage().getText().toLowerCase().equals("привет")) {
-            message.setText("Здарова");
-        } else {
-            message.setText("Ну ты и лох");
-        }
+                keyboardMarkup.setKeyboard(keyboard);
+                message.setReplyMarkup(keyboardMarkup);
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (messageText.equals("/hide")) {
+                SendMessage msg = new SendMessage().setChatId(chatId).setText("Keyboard hidden");
+                ReplyKeyboardRemove keyboardMarkup = new ReplyKeyboardRemove();
+                msg.setReplyMarkup(keyboardMarkup);
+                try {
+                    execute(msg);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                String userFirstName = update.getMessage().getChat().getFirstName();
+                String userLastName = update.getMessage().getChat().getLastName();
+                long userId = update.getMessage().getChat().getId();
+                String answer = EmojiParser.parseToUnicode("Here is a smile emoji: :smile:\n\n Here is alien emoji: :alien:");
+                SendMessage message = new SendMessage().setChatId(chatId).setText(answer);
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+            long chatId = update.getMessage().getChatId();
 
-
-        try {
-            execute(message);
-        } catch (TelegramApiException ex) {
-            ex.printStackTrace();
+            List<PhotoSize> photos = update.getMessage().getPhoto();
+            String photoId = photos.stream().sorted(Comparator.comparing(PhotoSize::getFileSize).reversed()).findFirst().orElse(null).getFileId();
+            int photoWidth = photos.stream().sorted(Comparator.comparing(PhotoSize::getFileSize).reversed()).findFirst().orElse(null).getWidth();
+            int photoHeight = photos.stream().sorted(Comparator.comparing(PhotoSize::getFileSize).reversed()).findFirst().orElse(null).getHeight();
+            String caption = "file_id: " + photoId + "\nwidth: " + Integer.toString(photoWidth) + "\nheight: " + Integer.toString(photoHeight);
+            SendPhoto msg = new SendPhoto().setChatId(chatId).setPhoto(photoId).setCaption(caption);
+            try {
+                execute(msg);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
     }
 
